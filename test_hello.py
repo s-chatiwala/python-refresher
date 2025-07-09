bb8d31b0fbb32d4c808d8348788c2da40277069d
Sabah Chatiwala <sabahchatiwala@gmail.com>
2025-07-09 16:45:41 -0400

    updated tests

--- test_hello.py
+++ test_hello.py
@@ -5,6 +5,39 @@ import hello
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello.hello(), "Hello, world!")
+        self.assertNotEqual(hello.hello(), "bye, world!")
+    
+    def test_add(self):
+        self.assertEqual(hello.add(6,7), 13)
+        self.assertEqual(hello.add(4,5), 9)
+        self.assertNotEqual(hello.add(6,7), 15)
+
+    def test_subtract(self):
+        self.assertEqual(hello.sub(7,5), 2)
+        self.assertNotEqual(hello.sub(7,5), 6)
+        self.assertEqual(hello.sub(7,3), 4)
+
+    def test_muliply(self):
+        self.assertEqual(hello.mul(2,3), 6)
+        self.assertEqual(hello.mul(2,4), 8)
+        self.assertNotEqual(hello.mul(2,3), 7)
+
+    def test_sqrt(self):
+        self.assertEqual(hello.sqrt(25), 5)
+        self.assertEqual(hello.sqrt(16), 4)
+        self.assertNotEqual(hello.sqrt(16), 5)
+    
+    def test_power(self):
+        self.assertEqual(hello.power(2,2), 4)
+        self.assertEqual(hello.power(3,3), 27)
+        self.assertNotEqual(hello.power(4,2), 15)
+
+    def test_log(self):
+        self.assertEqual(hello.log(100), 4.605170185988092)
+
+    def test_exp(self):
+        self.assertEqual(hello.exp(1), 2.718281828459045)
+
     def test_sin(self):
         self.assertEqual(hello.sin(0), 0)
         self.assertEqual(hello.sin(1), 0.8414709848078965)
